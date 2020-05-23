1f3ad3f96633d8d4e36ae4dbf95d2fa518d74e22
Michal Czaplinski <s20308@pjwstk.edu.pl>
2020-05-23 18:50:15 +0200

    dodano sprawdzenie ilosci lczb

--- src/main/java/param/pam/Main.java
+++ src/main/java/param/pam/Main.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String peselString = new String();
         System.out.println("podaj pesel: ");
-        Scanner scanner = new Scanner(System.in);
         peselString = scanner.nextLine();
-        if (PeselValidator.control(peselString)){
+        scanner.close();
+        if (11 == peselString.length() && PeselValidator.control(peselString)) {
             System.out.println("pesel prawidłowy");
         } else {
             System.out.println("pesel bledny");
-        };
+        }
 
     }
 }
--- src/main/java/param/pam/PeselValidator.java
+++ src/main/java/param/pam/PeselValidator.java
@@ -6,7 +6,6 @@ public class PeselValidator {
         int[] test = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
         for (int i = 0; i < 11; i++) {
             suma += (test[i] * pesel.codePointAt(i));
-            System.out.println(suma);
         }
         if (suma % 10 == 0) {
             return true;
